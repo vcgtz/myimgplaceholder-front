b7b89acf1a2456002fcc69fff3e73506d9f86d46
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-04-10 23:01:16 -0600

    Add build command

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}",
+    './src/**/*.{html,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
